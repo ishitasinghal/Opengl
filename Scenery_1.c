cd99ed0a42ebcf774fbf4737649ff18a2193e62d
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-11 21:49:38 +0530

    Commit 3.

--- Scenery_1.c
+++ Scenery_1.c
@@ -32,16 +32,95 @@ public void declare(char *s)
 public void objects()
 {
     int a;
-    if(day==1)
+    //making sky
+    glColor3f(0.0, 0.9, 0.9);
+    glBegin(GL_POLYGON);
+    glVertex2f(0,380);
+    glVertex2f(0,700);
+    glVertex2f(1100,700);
+    glVertex2f(1100,380);
+    glEnd();
+    
+    //making sun
+    for(a=0;a<25;a++)
     {
-        //making sky
-        glColor3f(0.0, 0.9, 0.9);
-        glBegin(GL_POLYGON);
-        glVertex2f(0,380);
-        glVertex2f(0,700);
-        glVertex2f(1100,700);
-        glVertex2f(1100,380);
-        glEnd();
+        glColor3f(1.0,0.9,0.0);
+        draw_circle(95,620,1);
     }
-}
+    
+    //making cloud
+    for(a=0;a<=20;a++)
+    {
+        glColor3f(1.0,1.0,1.0);
+        draw_circle(160+m,625,a);
+    }
+    
+    
+    for(a=0;a<=35;a++)
+    {
+        glColor3f(1.0,1.0,1.0);
+        draw_circle(200+m,625,a);
+        draw_circle(225+m,625,a);
+    }
+    
+    for(a=0;a<=20;a++)
+    {
+        glColor3f(1.0,1.0,1.0);
+        draw_circle(265+m,625,a);
+    }
+    
+    //making grass
+    glColor3f(0.6,0.8,0.196078);
+    glBegin(GL_POLYGON);
+    glVertex2f(0,160);
+    glVertex2f(0,380);
+    glVertex2f(1100,380);
+    glVertex2f(1100,160);
+    glEnd();
+    
+    //making tree 1
+    glColor3f(0.9,0.2,0.0);
+    glBegin(GL_POLYGON);
+    glVertex2f(100,135);
+    glVertex2f(100,285);
+    glVertex2f(140,285);
+    glVertex2f(140,135);
+    glEnd();
+    
+    
+    for(a=0;a<=40;a++)
+    {
+        glColor3f(0.0,0.5,0.0);
+        draw_circle(40,280,a);
+        draw_circle(90,280,a);
+        draw_circle(150,280,a);
+        draw_circle(210,280,a);
+        draw_circle(65,340,a);
+        draw_circle(115,340,a);
+        draw_circle(175,340,a);
+        
+    }
+    
+    for(a=0;a<=55;a++)
+    {
+        glColor3f(0.0,0.5,0.0);
+        draw_circle(115,360,a);
+    }
+    
+    //home
 
+    glColor3f(p,q,r);
+    glBegin(GL_POLYGON);
+    glVertex2f(550,100);
+    glVertex2f(550,330);
+    glVertex2f(950,330);
+    glVertex2f(950,100);
+    glVertex2f(850,100);
+    glVertex2f(850,250);
+    glVertex2f(650,250);
+    glVertex2f(650,100);
+    
+    glEnd();
+glFlush();
+    
+}
